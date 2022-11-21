802f0390e78bbd6af52abdbd3c6e6e79d46e00a8
tomislavantic <83822898+tomislavantic@users.noreply.github.com>
2022-11-21 16:24:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import opf_3ph_current_voltage
 net = pp.from_json(r'path\cigre_lv_modified.json')
 #Load curves that are input in pp OPF - both power-voltage and current-voltage formulations
 
-p_load_a = pd.read_excel(r'C:\Users\Tomislav\Desktop\Posao\PES GM 2023\p_load_a.xlsx', index_col = 0)
-p_load_b = pd.read_excel(r'C:\Users\Tomislav\Desktop\Posao\PES GM 2023\p_load_b.xlsx', index_col = 0)
-p_load_c = pd.read_excel(r'C:\Users\Tomislav\Desktop\Posao\PES GM 2023\p_load_c.xlsx', index_col = 0)
+p_load_a = pd.read_excel(r'path\p_load_a.xlsx', index_col = 0)
+p_load_b = pd.read_excel(r'path\p_load_b.xlsx', index_col = 0)
+p_load_c = pd.read_excel(r'path\p_load_c.xlsx', index_col = 0)
 
 # v_a, v_b, v_c = opf_3ph_power_voltage.opf_3ph_power_voltage(net, p_load_a, p_load_b, p_load_c, vm_pu = 1.00)
 v_a, v_b, v_c = opf_3ph_current_voltage.opf_3ph_current_voltage(net, p_load_a, p_load_b, p_load_c, vm_pu = 1.0)
